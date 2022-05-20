d0ec663dad71000c3a3da06056a96dcac13b1324
Evgeny Zakharov <risenforces@gmail.com>
2022-05-20 03:31:47 +0300

    feat: add next.js eslint plugin

--- src/presets/nextjs/index.ts
+++ src/presets/nextjs/index.ts
@@ -3,6 +3,7 @@ import { createPreset } from '../shared'
 export const nextJs = createPreset({
   name: 'next.js',
   compile: () => ({
+    extends: ['plugin:@next/next/recommended'],
     rules: {
       'import/no-default-export': 'off',
     },
